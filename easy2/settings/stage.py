dd6c05207fb32e9d54326aded15ef9d1175a19a1
Jim Buddin <jimbuddin@gmail.com>
2013-12-29 22:19:28 +0100

    Added the anon.user.png so it goes to the staging server Also updated the stage.py so that it explicitly states the SECRET_KEY. This is bad so TODO: Get the SECRET_KEY from env variable under Apache2/WSGI

--- easy2/settings/stage.py
+++ easy2/settings/stage.py
@@ -5,12 +5,12 @@ Django staging server (RaspberryPi) settings for easylaughs project.
 #from unipath import Path
 from .base import *
 
-SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
-
+#SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
+SECRET_KEY = "psst-this-is-my-secret-key-shhh"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
-
+	
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -31,10 +31,3 @@ DATABASES = {
 }
 
 ALLOWED_HOSTS = ['*']
-from django.conf.urls import patterns, include, url
-from easy2.urls import urlpatterns
-urlpatterns += patterns('',
-    (r'^media/(?P<path>.*)$',
-    'django.views.static.serve', {
-    'document_root': MEDIA_ROOT}),
-)
