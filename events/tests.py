89470197be4f3fecc6b400eaf55e97cb354cfae5
Jim Buddin <jimbuddin@gmail.com>
2014-01-03 02:17:34 +0100

    Added the little available / not available icons. Again.

--- events/tests.py
+++ events/tests.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
 
+import datetime
+from django.utils import timezone
+
+from django.core.urlresolvers import reverse
+
+from .models import Show
+
 def create_show(show, date, long_desc):
 	"""
 	A reusable method to create a show object for testing
@@ -7,3 +14,10 @@ def create_show(show, date, long_desc):
 	return Show.objects.create(show=show,
         date=date, long_desc=long_desc)
         
+class IndexViewTests(TestCase):
+	def test_index_with_no_shows(self):
+		"""
+		if no Shows exist it should show a message 'No Shows planned'
+		"""
+		response = self.client.get(reverse('events:index'))
+		
