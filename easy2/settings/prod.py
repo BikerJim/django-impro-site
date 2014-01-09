e173fab5819dc2521364a68e4572ea1a84f4e7d6
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 00:29:32 +0100

    trying to get heroku working

--- easy2/settings/prod.py
+++ easy2/settings/prod.py
@@ -4,14 +4,18 @@ Django production settings for easylaughs project.
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from .base import *
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
-DB_PASSWORD = get_env_variable("DB_PASSWORD")
-DB_NAME = get_env_variable("DB_NAME")
-DB_USER = get_env_variable("DB_USER")
+#SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
+#DB_PASSWORD = get_env_variable("DB_PASSWORD")
+#DB_NAME = get_env_variable("DB_NAME")
+#DB_USER = get_env_variable("DB_USER")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -26,14 +30,14 @@ ADMINS = (
 
 #EMAIL_HOST = 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-ALLOWED_HOSTS = ['localhost']
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'DB_NAME',
+#        'USER': 'DB_USER',
+#        'PASSWORD': 'DB_PASSWORD',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
+ALLOWED_HOSTS = ['*']
