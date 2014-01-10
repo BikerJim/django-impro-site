eb9046cf8a015d3474443c6703ef240b2c6da452
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 15:42:19 +0100

    added amazon AWS bucket for staticfiles

--- easy2/settings/prod.py
+++ easy2/settings/prod.py
@@ -23,3 +23,9 @@ ADMINS = (
 	('Jim Buddin', 'jimbuddin@hotmail.com'),
 )
 ALLOWED_HOSTS = ['*']
+INSTALLED_APPS += ('storages',)
+
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
