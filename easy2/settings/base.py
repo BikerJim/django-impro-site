6b535ec79ac3430b5d435199d9f10fd80a17a39f
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 01:06:36 +0100

    set SECRET_KEY in local bashrc

--- easy2/settings/base.py
+++ easy2/settings/base.py
@@ -14,7 +14,7 @@ from django.core.exceptions import ImproperlyConfigured
 def get_env_variable(var_name):
 	""" Get the env variable or return exception """
 	try:
-		return os.environ.get(var_name,'')
+		return os.environ[var_name]
 	except KeyError:
 		error_msg = "Set the %s environment variable" % var_name
 		raise ImproperlyConfigured(error_msg)
