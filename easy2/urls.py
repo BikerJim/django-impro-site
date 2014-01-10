a0c96073274a8e4bf2845388efaa2adc791a5851
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 16:20:20 +0100

    changed admin url to easylaughsrearend

--- easy2/urls.py
+++ easy2/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', Index.as_view(), name='index'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^easylaughsrearend/', include(admin.site.urls)),
     url(r'^about/', AboutUs.as_view(), name='about_us'),
     (r'^accounts/logout/$','django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/',include('allauth.urls')),
