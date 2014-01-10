491399096403e25bdb64a45705332c158d0ceb37
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 12:41:44 +0100

    added settings.base to get STATIC_ROOT

--- easy2/urls.py
+++ easy2/urls.py
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
 )
 if not settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.base.STATIC_ROOT}),
     )
