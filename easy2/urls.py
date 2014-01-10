ec4b6c3caa42cdb8f2a6077e1bbe0ceaed176ceb
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 12:53:22 +0100

    changed  staticfiles -> static and  removed the .base in STATIC_FILES path (urls)

--- easy2/urls.py
+++ easy2/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
 )
 if not settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.base.STATIC_ROOT}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
