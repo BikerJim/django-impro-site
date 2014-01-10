9465fd61dcc762ac146a5257adbba4ce31fc5568
Jim Buddin <jimbuddin@gmail.com>
2014-01-10 15:59:50 +0100

    added AWS settings and added keys etc to env variables

--- easy2/settings/prod.py
+++ easy2/settings/prod.py
@@ -25,7 +25,11 @@ ADMINS = (
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS += ('storages',)
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
