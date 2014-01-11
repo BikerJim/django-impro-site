3832d613be5d1f68d11c0422c7f8f8c4638bed99
Jim Buddin <jimbuddin@gmail.com>
2014-01-11 13:46:34 +0100

    testing media&static roots

--- easy2/settings/prod.py
+++ easy2/settings/prod.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS += ('storages',)
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 DEFAULT_FILE_STORAGE = 'easy2.settings.s3utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'easy2.settings.s3utils.StaticRootS3BotoStorage'
@@ -31,9 +32,9 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_DIRECTORY = '/media/'
 STATIC_URL = S3_URL
 MEDIA_URL = S3_URL + MEDIA_DIRECTORY
-#MEDIA_URL = MEDIA_ROOT
+MEDIA_ROOT = MEDIA_URL 
